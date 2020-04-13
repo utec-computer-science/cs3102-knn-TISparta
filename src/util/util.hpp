2e5e425f639f435e4002d99acf0eb0d3b05d9c33
TISparta <jose.garcia@utec.edu.pe>
2020-04-12 19:26:06 -0500

    test(cnn)

--- src/util/util.hpp
+++ src/util/util.hpp
@@ -1,8 +1,19 @@
+#include <cmath>
+#include <stdexcept>
+#include <utility>
+#include <algorithm>
+
 #include "../random/Random.hpp"
 #include "../point/Point.hpp"
 
+/*
+ * Utility functions
+ */
 namespace util {
 
+  /**
+   * Generate a random point
+   */
   template <typename value_t>
   static Point <value_t> generate_random_point (const value_t min_val, const value_t max_val) {
     value_t x = Random::generate_value <value_t> (min_val, max_val);
@@ -10,14 +21,44 @@ namespace util {
     return Point <value_t> (x, y);
   }
 
+  /*
+   * Generate n random points
+   */
   template <typename value_t>
   static std::vector <Point <value_t>> generate_n_random_points (const int n,
                                                                  const value_t min_val,
                                                                  const value_t max_val) {
+    if (n < 0) {
+      throw std::out_of_range("n points must be non negative");
+    }
     std::vector <Point <value_t>> points(n);
     for (int i = 0; i < n; i++) {
       points[i] = generate_random_point <value_t> (min_val, max_val);
     }
     return points;
+  }
+
+  /*
+   * Find the k-nearest neighbors of point center
+   */
+  template <typename value_t>
+  static std::vector <Point <value_t>> knn (int k,
+                                            std::vector <Point <value_t>> points,
+                                            Point <value_t> center) {
+    int n = points.size();
+    std::vector <std::pair <value_t, int>> dist_index(n);
+    for (int i = 0; i < n; i++) {
+      value_t x = points[i].get_x();
+      value_t y = points[i].get_y();
+      value_t d = std::pow(x - center.get_x(), 2) + std::pow(y - center.get_y(), 2); 
+      dist_index[i] = {d, i};
+    }
+    std::sort(std::begin(dist_index), std::end(dist_index));
+    k = std::min(k, n);
+    std::vector <Point <value_t>> ret(k);
+    for (int i = 0; i < k; i++) {
+      ret[i] = points[dist_index[i].second];
+    }
+    return ret;
   }
 };
--- test/test.cpp
+++ test/test.cpp
@@ -10,12 +10,52 @@ const value_t MIN_VAL = -100;
 const value_t MAX_VAL = +100;
 
 TEST(util, random_points_generation) {
-  int n = 10;
+  const int n = 10;
   std::vector <Point <value_t>> points = util::generate_n_random_points <value_t> (n, MIN_VAL, MAX_VAL);
   for (auto point: points) {
     std::cout << point;
   }
 }
+
+TEST(util, negative_argument_in_n_points) {
+  const int n = -1;
+  EXPECT_ANY_THROW({
+      std::vector <Point <value_t>> points = util::generate_n_random_points <value_t> (n, MIN_VAL, MAX_VAL);
+  });
+}
+
+TEST(util, knn_test_1) {
+  const int n = 5;
+  std::vector <Point <value_t>> points(n);
+  points[0] = Point <value_t> (0, 0);
+  points[1] = Point <value_t> (1, 1);
+  points[2] = Point <value_t> (2, 2);
+  points[3] = Point <value_t> (3, 3);
+  points[4] = Point <value_t> (4, 4);
+  const int k = 3;
+  Point <value_t> center(2, 2);
+  std::vector <Point <value_t>> knn = util::knn(k, points, center);
+  for (auto p: knn) {
+    std::cout << p;
+  }
+}
+
+TEST(util, knn_test_2) {
+  const int n = 5;
+  std::vector <Point <value_t>> points(n);
+  points[0] = Point <value_t> (0, 0);
+  points[1] = Point <value_t> (1, 1);
+  points[2] = Point <value_t> (2, 2);
+  points[3] = Point <value_t> (3, 3);
+  points[4] = Point <value_t> (4, 4);
+  const int k = 3;
+  Point <value_t> center(5, 5);
+  std::vector <Point <value_t>> knn = util::knn(k, points, center);
+  for (auto p: knn) {
+    std::cout << p;
+  }
+}
+
 
 int main (int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
